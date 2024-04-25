78d80a39a1db18c6f721aaef05c3da925c8a5627
Zigglez <58371260+Zigglez@users.noreply.github.com>
2024-04-25 12:56:45 +0100

    Create arabic.py

---
+++ arabic.py
@@ -0,0 +1,29 @@
+# Change original to be the alphabet you want for example
+# original = ['a', 'b', 'c', 'd', 'e', 'f']
+
+# This just spawns 180 consecutive hexadecimal numbers, comment it and replace with your alphabet
+original = [hex(i) for i in range(1,181)]
+
+
+# DO NOT CHANGE ANYTHING BELOW
+
+def reverse(a):
+    if len(a) == 0:
+        return []
+    elif len(a) == 1:
+        return a
+    else:
+        return reverse(a[1:-1]) + [a[len(a)-1], a[0]]
+
+prev = original
+new = reverse(original)
+count = 1
+
+while new != original:
+    print(f"Row: {count} -> {prev[0:50]}...")
+    prev = new
+    new = reverse(prev)
+    count += 1
+
+print(f"Row: {count} -> {prev[0:50]}...")
+print(f"Row: {count+1} -> {new}")
