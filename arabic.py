51e67f4b922f5f632178361d3bdf2f22a9b8be28
Zigglez <58371260+Zigglez@users.noreply.github.com>
2024-04-26 15:18:32 +0100

    Update arabic.py

--- arabic.py
+++ arabic.py
@@ -4,6 +4,7 @@
 # This just spawns 180 consecutive hexadecimal numbers, comment it and replace with your alphabet
 original = [hex(i) for i in range(1,181)]
 
+م ح م د ا د م ا ب ر ا ه ي م ي و س ف د ا و د س ل ي م ن ع ي س ى ك ي و م ر ث ج م ش ي د ف ر ي د و ن ك س ر ي س ك ن د ر ن و ش ي ر و ا ن م و س ى س ا ج د ن و ر ج ح ا ن ز ه ل م ش ت ر ي م ر ي خ ش م س ز ه ر ة ع ط ا ر د ق م ر ج ب ر ا ا ي ل م ي ك ا ا ي ل ا س ر ا ف ي ل ع ز ر ا ا ي ل د ر د ا ا ي ل ر ف ت م ا ا ي ل م ي ط ط ر و ن
 
 # DO NOT CHANGE ANYTHING BELOW
 
